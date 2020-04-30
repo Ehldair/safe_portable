2e4c497a9ed06002b9d8a833a0c2497ac719de4c
onth <onth@192.168.1.129>
2020-04-30 12:11:44 +0200

    cambios dia 30

--- inicio.php
+++ inicio.php
@@ -39,19 +39,7 @@ if(isset($_SESSION['id_u'])) {
     		<script>
     		function respuesta() {
     			var respuesta=<?php echo $respuesta; ?>;
-    			if(respuesta==1) {
-    				var c = document.getElementById("mensaje");
-    				var ctx = c.getContext("2d");
-    				ctx.font = "bold 12px Verdana";
-    				ctx.clearRect(0, 0, c.width, c.height);
-    				ctx.strokeStyle = "#3DBA26";
-    				ctx.strokeRect(1, 1, 299, 29);
-    				ctx.fillStyle = "#3DBA26";
-    				ctx.textAlign = "center";
-    				ctx.fillText("Caso creado",150,20);
-    				<?php $_SESSION['respuesta']=0; ?>
-    			}
-    			else {
+
     				if(respuesta==2) {
     					var c = document.getElementById("mensaje");
     					var ctx = c.getContext("2d");
@@ -64,7 +52,6 @@ if(isset($_SESSION['id_u'])) {
     					ctx.fillText("Caso eliminado",150,20);
     					<?php $_SESSION['respuesta']=0; ?>
     				}
-    			}
     	        
     	      }
     		</script>
@@ -85,34 +72,6 @@ if(isset($_SESSION['id_u'])) {
     			            }
     			        });
     			    });
-    			 
-    			    $('#btn2').on('click', function(){
-    			    	var c = document.getElementById("mensaje");
-    					var ctx = c.getContext("2d");
-    			    	ctx.clearRect(0, 0, c.width, c.height);
-    			        $.ajax({
-    			            type: "POST",
-    			            url: "cerrados.php",
-    			          
-    			            success: function(response) {
-    			                $('#div-results').html(response);
-    			            }
-    			        });
-    			    });
-    			    $('#btn3').on('click', function(){
-    			    	var c = document.getElementById("mensaje");
-    					var ctx = c.getContext("2d");
-    			    	ctx.clearRect(0, 0, c.width, c.height);
-    			        $.ajax({
-    			            type: "POST",
-    			            url: "todos.php",
-    			          
-    			            success: function(response) {
-    			                $('#div-results').html(response);
-    			                
-    			            }
-    			        });
-    			    });
     			    $('#btn4').on('click', function(){
     			    	var c = document.getElementById("mensaje");
     					var ctx = c.getContext("2d");
@@ -126,49 +85,7 @@ if(isset($_SESSION['id_u'])) {
     			                
     			            }
     			        });
-    			    });
-    			    $('#btn5').on('click', function(){
-    			    	var c = document.getElementById("mensaje");
-    					var ctx = c.getContext("2d");
-    			    	ctx.clearRect(0, 0, c.width, c.height);
-    			        $.ajax({
-    			            type: "POST",
-    			            url: "busqueda_caso.php",
-    			       
-    			            success: function(response) {
-    			                $('#div-results').html(response);
-    			                
-    			            }
-    			        });
-    			    });
-    			    $('#btn6').on('click', function(){
-    			    	var c = document.getElementById("mensaje");
-    					var ctx = c.getContext("2d");
-    			    	ctx.clearRect(0, 0, c.width, c.height);
-    			        $.ajax({
-    			            type: "POST",
-    			            url: "informes.php",
-    			       
-    			            success: function(response) {
-    			                $('#div-results').html(response);
-    			                
-    			            }
-    			        });
-    			    });
-    			    $('#btn7').on('click', function(){
-    			    	var c = document.getElementById("mensaje");
-    					var ctx = c.getContext("2d");
-    			    	ctx.clearRect(0, 0, c.width, c.height);
-    			        $.ajax({
-    			            type: "POST",
-    			            url: "estadisticas.php",
-    			       
-    			            success: function(response) {
-    			                $('#div-results').html(response);
-    			                
-    			            }
-    			        });
-    			    });
+    			    });  
     			});
     
     
@@ -181,7 +98,7 @@ if(isset($_SESSION['id_u'])) {
     
     			<!-- Header -->
     				<header id="header">
-    					<h1><a href="login.php">Safe Ciber</a> Gesti&oacuten Secci&oacuten Ciberterrorismo</h1>
+    					<h1><a href="login.php">Safe ver. PORTABLE</a> Gesti&oacuten Secci&oacuten Ciberterrorismo</h1>
     					<nav id="nav">
     						<ul>
     							<li><a href="inicio.php">Home</a></li>
@@ -195,8 +112,6 @@ if(isset($_SESSION['id_u'])) {
     										<a href="#">Listar</a>
     										<ul>
     											<li><a href="#">Abiertos</a></li>
-    											<li><a href="#">Cerrados</a></li>
-    											<li><a href="#">Todos</a></li>
     										</ul>
     									</li>
     								</ul>
@@ -217,41 +132,20 @@ if(isset($_SESSION['id_u'])) {
     						<div class="col-12">	
     									<ul class="actions special">
     										<li><input type="button" id="btn4" value="Nuevo Caso"></li>
-    										<li><input type="button" id="btn1" value="Casos Abiertos"></li>
-    										<li><input type="button" id="btn2" value="Casos Cerrados"></li>
-    										<li><input type="button" id="btn3" value="Todos Casos"></li>
+    										<li><input type="button" id="btn1" value="Casos Grabados"></li>
     										
     										
     									</ul>
     									
     						</div>
     						<div class="col-12">	
-    									<ul class="actions special">
-    										<li><input type="button" id="btn5" value="Buscar Casos"></li>
-    										<li><input type="button" id="btn6" value="Informes"></li>
-    										
-    										<li><input type="button" id="btn7" value="Estadísticas"></li>
-    										
-    										<li>
-    									      
-    <?php 
-                                          
-    											if ($_SESSION['admin']==2) {		  
-    		   										echo "<form action='admin.php' method='post'>";
-    												echo" <input type='submit' value='Administración'>";
-    												echo "</form>";    
-    											}
-    											else {
-    												echo" <input type='button' value='Administrador' class='button disabled'>";
-    											} 	
+ <?php
 }
 else {
  echo "   Error";
 }
 ?>
-								
-										</li>									
-									</ul>
+
 			
 			<div id="div-results"></div>
 				<form>
