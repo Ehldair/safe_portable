0f017819bb37203b908862ef934d763f49fcad16
Ehldair <onth@hotmail.com>
2020-05-05 09:14:55 +0200

    cambios dia 5

---
+++ login.php
@@ -0,0 +1,77 @@
+<html>
+	<head>
+		<title>SAFE Ciberterrorismo Comisar�a General de Informaci�n UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="landing is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header menus de la cabecera-->
+				<header id="header" class="alt">
+					<h1><a href="login.php">Safe Ciber</a> Comisar&iacute;a General de Informaci&oacute;n</h1>					
+				</header>
+
+			<!-- Login -->
+				<section id="banner">
+				
+					<h2>Safe Ciber</h2> 
+					<p>Gestión Sección Ciberterrorismo</p>
+					
+					
+        				<form action="formpost.php" method="post">				
+            				<div class="row gtr-50 gtr-uniform">
+            				 
+        						<div class="col-6-mobilep">
+        							<input type="text" name="nombre" id="name" placeholder="User" />
+        						</div>
+        						<div class="col-6-mobilep">        							
+        							<input type="password" name="password" id="passwordlogin" placeholder="Pass" />        							
+        						</div>        					 
+        													
+        						<div class="col-12" >
+        							<ul class="actions special">
+        								<li><input type="submit" value="Entrar" class="button"></li>
+        							</ul>
+        						</div>
+        					</div>    					    					
+          				</form>	     				  	        				
+          	
+				</section>			
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon brands fa-google-plus"><span class="label">Google+</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. Uso exclusivo de ciber.</li><li>Design: </li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+			
+		<!-- Propios -->
+			<script src="//code.jquery.com/jquery-latest.js"></script>
+ 	 		<script src="miscript.js"></script>
+  			<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+  			<script src="js/jquery-3.4.1.js"></script>
+
+	</body>
+</html>
+>>>>>>> branch 'master' of https://github.com/pelayocibers/CiberSafe.git
