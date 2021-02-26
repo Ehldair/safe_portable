e49fef2b01eb51324cc86dfefa51dcce1db7e82d
Analista <Analista@DESKTOP-2O7153T>
2021-02-26 10:33:29 +0100

    cambios dia 26F

--- eliminarevidencia.php
+++ eliminarevidencia.php
@@ -5,20 +5,21 @@ $link = mysqli_connect("localhost", "root", ".google.", "safe_portable");
 
 
 $myid_ev=mysqli_real_escape_string($link,$_POST["category"]);
-   
-    //ELIMINA LA EVIDENCIA SI NO TIENE ASOCIADAS 
-    //Primero elimina los registros asociados de la evidencia en la tabla evidencia_registro
-    
-    $resultado_ev_reg=mysqli_query($link, "SELECT * from evidencia_registro where id_evidencia=$myid_ev");
-    $num_ev_reg=mysqli_num_rows($resultado_ev_reg);
-    if($num_ev_reg!=0) {
-        while ($line_evidencias = mysqli_fetch_array($resultado_ev_reg, MYSQLI_ASSOC)) {
-            $id_evidencia_reg = $line_evidencias['id_evidencia'];
-            
-            $sql="DELETE FROM evidencia_registro where id_evidencia=$id_evidencia_reg";
-            $result = mysqli_query($link,$sql);
-            if (!$result) {
-                ?>
+
+
+//ELIMINA LA EVIDENCIA SI NO TIENE ASOCIADAS
+//Primero elimina los registros asociados de la evidencia en la tabla evidencia_registro
+
+$resultado_ev_reg=mysqli_query($link, "SELECT * from evidencia_registro where id_evidencia=$myid_ev");
+$num_ev_reg=mysqli_num_rows($resultado_ev_reg);
+if($num_ev_reg!=0) {
+    while ($line_evidencias = mysqli_fetch_array($resultado_ev_reg, MYSQLI_ASSOC)) {
+        $id_evidencia_reg = $line_evidencias['id_evidencia'];
+        
+        $sql="DELETE FROM evidencia_registro where id_evidencia=$id_evidencia_reg";
+        $result = mysqli_query($link,$sql);
+        if (!$result) {
+            ?>
          
              <script type="text/javascript">
                 alert("Fallo al eliminar la evidencia de la tabla : evidencia_registro.\n\n Detalles: \n\n <?php echo  mysqli_error($link) ?>");
@@ -52,18 +53,21 @@ $myid_ev=mysqli_real_escape_string($link,$_POST["category"]);
     $sql="Select relacionado_con from evidencia where id_evidencia=$myid_ev";
     $resultado=mysqli_query($link, $sql);
     $ret=mysqli_fetch_array($resultado);
-    
-    $sql="Select * from evidencia where relacionado_con=$ret[relacionado_con] AND id_evidencia!=$myid_ev";
-    $resultado=mysqli_query($link, $sql);
-    $count=mysqli_num_rows($resultado);
-    if($count==0) {
-        $sql="Update Evidencia set tiene_subevidencias=0 where id_evidencia=$ret[relacionado_con]";
-        mysqli_query($link, $sql);
+    if($ret['relacionado_con']!=null && $ret['relacionado_con']!='null' && $ret['relacionado_con']!='') {
+   
+         $sql="Select * from evidencia where relacionado_con='$ret[relacionado_con]' AND id_evidencia!=$myid_ev";
+        $resultado=mysqli_query($link, $sql);
+        $count=mysqli_num_rows($resultado);
+        if($count==0) {
+            $sql="Update Evidencia set tiene_subevidencias=0 where id_evidencia=$ret[relacionado_con]";
+            mysqli_query($link, $sql);
+        }
     }
            
     //Tercero elimina la evidencia de la tabla evidencia
     
     $sql="DELETE FROM evidencia where id_evidencia=$myid_ev";
+    echo $sql;
     $result = mysqli_query($link,$sql);
     if (!$result) {
         ?>
@@ -75,11 +79,11 @@ $myid_ev=mysqli_real_escape_string($link,$_POST["category"]);
     
     <?php
     }
-    else
+    /*else
     {
         $_SESSION['respuesta']=7;
         echo 7;
-    }
+    }*/
 
 
 ?>
